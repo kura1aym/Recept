bd0b19b042c975a0cb0b350b24cbbcea1c964f98
Nuray <210103331@stu.sdu.edu.kz>
2023-12-17 18:57:15 +0600

    for showing ui screen

--- app/src/main/java/com/example/recipeapp/ui/recipe_screen/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipe_screen/RecipeScreen.kt
@@ -1,16 +1,22 @@
 package com.example.recipeapp.ui.recipe_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DownloadForOffline
@@ -20,12 +26,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import com.example.recipeapp.ui.theme.MyPadding
+import com.example.recipeapp.ui.theme.lemonMilkFonts
 
 @Composable
 fun RecipeScreen(){
+
+
 
     val scaffoldState = rememberScaffoldState()
 
@@ -80,13 +95,95 @@ fun RecipeScreen(){
 
                             IconButton(
                                 onClick ={}
-                            ) {
+                            )  {
 
                             }
                         }
                     }
 
+                    SubcomposeAsyncImage(
+                        model ="",
+                        loading = {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(50.dp),
+                                color = MaterialTheme.colors.primaryVariant
+                            )
+                        },
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .graphicsLayer {
+                                this.alpha = 0.25f
+                                shadowElevation = 8.dp.toPx()
+                                clip = true
+                            }
+                            .align(Alignment.Center),
+                        contentScale = ContentScale.Crop,
+                        filterQuality = FilterQuality.Medium
+                    )
+                    Text(
+                        text = "",
+                        style = MaterialTheme.typography.h4,
+                        fontWeight = FontWeight.ExtraLight,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colors.onSurface,
+                        fontFamily = lemonMilkFonts,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+//                        Spacer(modifier = Modifier.height(MyPadding.medium))
+            }
 
+            item {
+                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        text = "Download Ingredients",
+                        fontFamily = lemonMilkFonts,
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier.padding(horizontal = MyPadding.medium)
+                            .clickable{
+
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(MyPadding.medium))
+                    IconButton(onClick = {
+                    }) {
+                        Icon(imageVector = Icons.Default.DownloadForOffline,
+                            contentDescription = "download recipe")
+                    }
+                }
+                Spacer(modifier = Modifier.height(MyPadding.medium))
+            }
+
+
+
+            item {
+                Text(
+                    text = "Ingredients",
+                    fontFamily = lemonMilkFonts,
+                    fontWeight = FontWeight.Medium,
+                    style = MaterialTheme.typography.h4,
+                    modifier = Modifier.padding(horizontal = MyPadding.medium)
+                )
+                Spacer(modifier = Modifier.height(MyPadding.medium))
+            }
+
+
+
+            item {
+                Text(
+                    text = "Method",
+                    fontFamily = lemonMilkFonts,
+                    fontWeight = FontWeight.Medium,
+                    style = MaterialTheme.typography.h4,
+                    modifier = Modifier.padding(horizontal = MyPadding.medium)
+                )
+                Spacer(modifier = Modifier.height(MyPadding.medium))
+            }
+
+
+        }
+    }
 }
-}
-}}}
+
