ed8533fb1525e34bec54364ac98b5ce3a72d516b
Yerlan Jumabayev <thevarp18@gmail.com>
2023-12-17 18:04:36 +0600

    create data folder and init converter

---
+++ app/src/main/java/com/example/recipeapp/core/Font.kt
@@ -0,0 +1,14 @@
+
+package com.example.recipeapp.core
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.recipeapp.R
+
+val lemonMilkFonts = FontFamily(
+    Font(R.font.lemonmilkproultrathin, weight = FontWeight.ExtraLight),
+    Font(R.font.lemonmilkproregular, weight = FontWeight.Normal),
+    Font(R.font.lemonmilkpromedium, weight = FontWeight.Medium),
+    Font(R.font.lemonmilkprobold, weight = FontWeight.Bold)
+)
