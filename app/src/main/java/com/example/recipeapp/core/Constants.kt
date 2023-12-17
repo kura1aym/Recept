b3ff90ac9c161f962bd71fa43b49d0ab823aee92
Yerlan Jumabayev <thevarp18@gmail.com>
2023-12-17 17:27:10 +0600

    add my api url

--- app/src/main/java/com/example/recipeapp/core/Constants.kt
+++ app/src/main/java/com/example/recipeapp/core/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.recipeapp.core
 
 object Constants {
-    const val BASE_URL = "https://recipeapi.github.io/api/"
+    const val BASE_URL = "https://thevarp19.github.io/recipe_my_api/"
     const val RECIPE_SCREEN_RECIPE_TITLE_KEY = "recipetitle"
     const val RECIPE_SCREEN_RECIPE_CATEGORY_KEY = "recipecategory"
     const val RECIPE_SCREEN_SHOULD_LOAD_FROM_SAVED_RECIPES = "shouldloadfromsavedrecipes"
