09efb051799766b2bea350f96871a55ece7fcd08
Nuray <210103331@stu.sdu.edu.kz>
2023-12-17 18:45:33 +0600

    starter ui for recipe screen

--- app/src/main/java/com/example/recipeapp/ui/recipe_screen/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipe_screen/RecipeScreen.kt
@@ -1,13 +1,22 @@
 package com.example.recipeapp.ui.recipe_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DownloadForOffline
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
@@ -30,15 +39,54 @@ fun RecipeScreen(){
                         .height(LocalConfiguration.current.screenHeightDp.dp / 2)
                         .graphicsLayer {
                             shadowElevation = 8.dp.toPx()
-                          //  shape = CustomShape()
+                            //  shape = CustomShape()
                             clip = true
                         }
                         .drawBehind {
                             drawRect(color = Color(0xFF000000))
                         },
-                )
-            }
+                ){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                            .drawBehind { drawRect(color = Color.Transparent) },
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(
+                            onClick = { },
+                            modifier = Modifier.padding(4.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = "goto home screen",
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(4.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+
+                            IconButton(onClick = {
+                            }) {
+                                Icon(imageVector = Icons.Default.DownloadForOffline,
+                                    contentDescription = "download recipe")
+                            }
+
+                            IconButton(
+                                onClick ={}
+                            ) {
+
+                            }
+                        }
+                    }
 
 
-        }
 }
+}
+}}}
