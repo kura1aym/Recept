f2039e658c9090898e3e79dcea939f4cf307ec93
Nuray <210103331@stu.sdu.edu.kz>
2023-12-18 21:40:53 +0600

    viewmodel for recipe list and interfaces

--- app/src/main/java/com/example/recipeapp/ui/recipe_list_screen/RecipeListViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipe_list_screen/RecipeListViewModel.kt
@@ -1,11 +1,41 @@
 package com.example.recipeapp.ui.recipe_list_screen
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class RecipeListViewModel @Inject constructor(
 
 ) : ViewModel(){
     val isEditModeOn = mutableStateOf<Boolean>(false)
+    private val _searchBoxState = mutableStateOf("")
+    val searchBoxState: State<String> = _searchBoxState
+    var searchJob: Job? = null
+
+    fun onSearchBoxValueChanged(newValue: String) {
+
+    }
+    private fun searchRecipe() {
+
+    }
+    fun onClearSearchBoxButtonClicked() {}
+
+    fun onSelectRadioButtonClicked(){}
+    fun receiveFromRecipeListScreenEvents(){}
+}
+
+sealed interface ToRecipeListScreenEvents{
+    object NavigateUp: ToRecipeListScreenEvents
+    class ShowSnackbar(val message: String): ToRecipeListScreenEvents
+}
+
+sealed interface FromRecipeListScreenEvents{
+    object DisableEditMode: FromRecipeListScreenEvents
+    object DeleteButtonClicked: FromRecipeListScreenEvents
+    object ChangeRefreshState : FromRecipeListScreenEvents
 }
