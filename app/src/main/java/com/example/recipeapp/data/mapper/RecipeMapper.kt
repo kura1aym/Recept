b7adccccfae48f1f9a566b786e5743bfa27dbf55
Nuray <210103331@stu.sdu.edu.kz>
2023-12-19 01:28:08 +0600

    entity for recipe

--- app/src/main/java/com/example/recipeapp/data/mapper/RecipeMapper.kt
+++ app/src/main/java/com/example/recipeapp/data/mapper/RecipeMapper.kt
@@ -1,7 +1,11 @@
 package com.example.recipeapp.data.mapper
 
+import com.example.recipeapp.data.local.LocalRecipeCategoryEntity
+import com.example.recipeapp.data.local.LocalRecipeEntity
 import com.example.recipeapp.data.local.RecipeEntity
+import com.example.recipeapp.data.remote.dto.categories.CategoryDtoItem
 import com.example.recipeapp.data.remote.dto.recipes.RecipeDtoItem
+import com.example.recipeapp.model.ModelLocalRecipe
 
 
 fun RecipeEntity.toRecipeDtoItem(): RecipeDtoItem = RecipeDtoItem(
@@ -18,4 +22,47 @@ fun RecipeDtoItem.toRecipeEntity(): RecipeEntity = RecipeEntity(
     method = method,
     tag = tag,
     title = title
+)
+
+
+fun RecipeDtoItem.toLocalRecipeEntity(): LocalRecipeEntity = LocalRecipeEntity(
+    imageUrl = imageUrl,
+    ingredient = ingredient,
+    method = method,
+    tag = tag,
+    title = title
+)
+
+fun RecipeEntity.toLocalRecipeEntity(): LocalRecipeEntity = LocalRecipeEntity(
+    imageUrl = imageUrl,
+    ingredient = ingredient,
+    method = method,
+    tag = tag,
+    title = title
+)
+
+fun LocalRecipeEntity.toModelLocalRecipe(): ModelLocalRecipe = ModelLocalRecipe(
+    imageUrl = imageUrl,
+    ingredient = ingredient,
+    method = method,
+    tag = tag,
+    title = title
+)
+
+fun ModelLocalRecipe.toRecipeDtoItem(): RecipeDtoItem = RecipeDtoItem(
+    imageUrl = imageUrl,
+    ingredient = ingredient,
+    method = method,
+    tag = tag,
+    title = title
+)
+
+fun CategoryDtoItem.toLocalRecipeCategoryEntity(): LocalRecipeCategoryEntity = LocalRecipeCategoryEntity(
+    category = category,
+    imageUrl = imageUrl
+)
+
+fun LocalRecipeCategoryEntity.toCategoryDtoItem(): CategoryDtoItem = CategoryDtoItem(
+    category = category,
+    imageUrl = imageUrl
 )
