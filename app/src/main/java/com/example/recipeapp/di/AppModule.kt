23785e1919c41d60b5abd313d4086744882ea145
Nuray <210103331@stu.sdu.edu.kz>
2023-12-19 03:21:58 +0600

    s

---
+++ app/src/main/java/com/example/recipeapp/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.recipeapp.di
+
+
+import android.app.Application
+import androidx.room.Room
+import com.example.recipeapp.core.Constants
+import com.example.recipeapp.data.local.RecipeDatabase
+import com.example.recipeapp.data.remote.RecipeApi
+import com.example.recipeapp.data.remote.repository.RecipeRepositoryImpl
+import com.example.recipeapp.domain.repository.RecipeRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesRecipeApi(): RecipeApi = Retrofit
+        .Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(RecipeApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesRecipeDatabase(app: Application): RecipeDatabase = Room
+        .databaseBuilder(app, RecipeDatabase::class.java, RecipeDatabase.DATABASE_NAME)
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesRecipeRepository(recipeApi: RecipeApi, database: RecipeDatabase): RecipeRepository =
+        RecipeRepositoryImpl(recipeApi = recipeApi, recipeDatabase = database)
+}
